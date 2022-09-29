6a4fd91081e493a0c398f86c7a1deaa4c623f44b
Alexsis D <56886983+Lexid1622@users.noreply.github.com>
2022-09-29 18:20:32 -0400

    Buttons and pages for all functions. First page is login where tuning is free to use without an account. Next page has the other two features.

--- lib/main.dart
+++ lib/main.dart
@@ -27,23 +27,38 @@ class MyHomePage extends StatelessWidget {
   final String title;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return const Tuning(title: 'Tuning');
-            }));
-          },
-          child: const Text('Tuning'),
-        ),
+    final ButtonStyle style =
+    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const LockedFeatures(title: 'LockedFeatures');
+              }));
+            },
+            child: const Text('Login'),
+          ),
+
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const Tuning(title: 'Tuning');
+              }));
+            },
+            child: const Text('Tuning'),
+          ),
+        ],
       ),
     );
   }
 }
+
 
 class Tuning extends StatelessWidget {
   const Tuning({Key? key, required this.title}) : super(key: key);
@@ -68,6 +83,45 @@ class Tuning extends StatelessWidget {
 }
 
 
+class LockedFeatures extends StatelessWidget {
+  const LockedFeatures({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const Comp(title: 'Comp');
+              }));
+            },
+            child: const Text('Comp'),
+          ),
+
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const Int(title: 'Int');
+              }));
+            },
+            child: const Text('Int'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class Comp extends StatelessWidget {
   const Comp({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -83,7 +137,29 @@ class Comp extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-          child: const Text('Home'),
+          child: const Text('Back to LockedFeatures'),
+        ),
+      ),
+    );
+  }
+}
+
+class Int extends StatelessWidget {
+  const Int({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Back to LockedFeatures'),
         ),
       ),
     );
