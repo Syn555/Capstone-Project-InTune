92e1efd8347ce25fa38eaf7208033fddafa9408f
jason-zhang-github <89471571+jason-zhang-github@users.noreply.github.com>
2022-12-03 17:27:33 -0500

    import statements

--- test/on_change_practice_test.dart
+++ test/on_change_practice_test.dart
@@ -6,8 +6,12 @@ import 'package:mockito/mockito.dart';
 import 'package:capstone_project_intune/ui/updateProfile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
-import 'package:mocking/main.dart';
-import 'package:mockito/annotations.dart';
+// import 'package:mocking/main.dart';
+// import 'package:mockito/annotations.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:firebase_storage_mocks/firebase_storage_mocks.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 
 class MockFirebaseAuth extends Mock implements FirebaseAuth{}
 class MockFirebaseUser extends Mock implements FirebaseUser{}
